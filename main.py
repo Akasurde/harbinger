1be141d19ea09997134847988eca8d53f4d706fc
Abhijeet Kasurde <akasurde@redhat.com>
2020-04-06 22:34:40 +0530

    Added initial code
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+# Copyright: (c) 2020, Abhijeet Kasurde (@Akasurde) <akasurde@redhat.com>
+# MIT License (see LICENSE or https://opensource.org/licenses/MIT)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import argparse
+import os
+
+try:
+    import argcomplete
+except ImportError:
+    argcomplete = None
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Autogenerate modules from OpenAPI file'
+    )
+
+    # Debug
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help='Show all debug logs')
+
+    if argcomplete:
+        argcomplete.autocomplete(parser)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+    product_dir = "./products/"
+    
+
+
+if __name__ == '__main__':
+    main()
